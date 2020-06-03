f08c9352fb46ed1d01274821a56accca196ef392
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-03 16:25:18 +0100

    removed external file for db connection

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 import express from "express";
 import { json } from "body-parser";
-import { ObjectID as _ObjectID, MongoClient } from "mongodb";
+import mongoose from "mongoose";
 import contactController from "./express-app/controllers/contactController.js";
-import {mongoose} from ("./config/database.js");
 
 const app = express();
 app.use(json());
+
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if(err) {
+        console.log("DB connection error: ", err);
+        
+    }
+    
+    console.log("MongoDB conection success.");
+
+    const server = app.listen(process.env.PORT || 8080, () => {
+        const port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
 
 const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log("App now running on port", port);
 });
+
 app.use("/contacts", contactController)
