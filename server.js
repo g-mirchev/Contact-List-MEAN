e8ba4b837e0754e90ca48a6d33a02ad11f2e4bd5
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-04 15:50:42 +0100

    removed unnecessary import

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const contactController = require("./express-app/controllers/contactController.js");
 const routes = require("./express-app/routes/routes");
 
 /**
