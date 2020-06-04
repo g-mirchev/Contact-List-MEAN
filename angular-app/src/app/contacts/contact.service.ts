3f48725405f7732544c260f49babe9f928594648
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-04 18:50:55 +0100

    Created endpoints service

--- angular-app/src/app/contacts/contact.service.ts
+++ angular-app/src/app/contacts/contact.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import { Contact } from './contact';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ContactService {
 
-  constructor() { }
+  private contactsUrl = "/api/contacts";
+
+  constructor(private http: Http) {}
+
+  /**
+   * GET  ("/api/contacts")
+   */
+  getContents(): Promise<void | Contact[]> {
+    return this.http.get(this.contactsUrl)
+               .toPromise()
+               .then(response => response.json() as Contact[])
+               .catch(this.handleError);
+  }
+
+  /**
+   * POST   ("/api/contacts")
+   * 
+   * @param newContact Contact object to be sent
+   */
+  createContact(newContact: Contact): Promise<void | Contact> {
+    return this.http.post(this.contactsUrl, newContact)
+               .toPromise()
+               .then(response => response.json() as Contact)
+               .catch(this.handleError);
+  }
+
+  /**
+   * GET  ("/api/contacts/:id")
+   */
+
+   /**
+   * PUT  ("/api/contacts/:id")
+   */
+  updateContact(putContact: Contact): Promise<void | Contact> {
+    let putUrl = this.contactsUrl + '/' + putContact._id;
+    return this.http.put(putUrl, putContact)
+               .toPromise()
+               .then(response => response.json() as Contact)
+               .catch(this.handleError);
+  }
+
+   /**
+   * DELETE  ("/api/contacts/:id")
+   */
+  deleteContact(deleteContactId: String): Promise<void | String> {
+    return this.http.delete(this.contactsUrl + '/' + deleteContactId)
+               .toPromise()
+               .then(response => response.json() as String)
+               .catch(this.handleError);
+  }
+
+  /**
+   * Custom error handling
+   */
+  private handleError(error: any) {
+    let errorMessage = (error.message) ? error.message :
+    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errorMessage);
+  }
 }
