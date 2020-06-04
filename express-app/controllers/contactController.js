7f045c238d00af38d29347553dc3f6cc00ecaeea
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-04 17:12:12 +0100

    Implemented CRUD fuctions for contacts

--- express-app/controllers/contactController.js
+++ express-app/controllers/contactController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ObjectId = require('mongoose').ObjectId;
 const { Contact } = require("../models/contact");
 
 /**
@@ -10,19 +11,19 @@ function handleError(res, reason, message, code) {
 }
 
 /**
- * Export functions to be used by router.
+ * Export CRUD functions to be used by router
  */
 module.exports = {
 
     /**
-     * Returns all contacts from database.
+     * Reads all contacts from database.
      */
     all: function(req, res) {
         Contact.find({}, function(err, docs) {
             if (!err) {
                 res.status(200).json(docs);
             } else {
-                handleError(res, err.message, "Failed to get contacts.")
+                handleError(res, err.message, "Failed to get contacts.");
             }
         });
     },
@@ -41,8 +42,65 @@ module.exports = {
             if (!err) {
                 res.status(201).json(doc);
             } else {
-                handleError(res, err.message, "Failed to get contacts.");    
+                handleError(res, err.message, "Failed to create new contact.");    
             }
         });
+    },
+
+    /**
+     * Reads a contact with specific ID
+     */
+    getById: function(req, res) {
+        if(!ObjectId.isValid(req.params.id)) {
+            handleError(res, "Invalid ID", `No contact with given ID: ${req.params.id}`, 400);
+        } else {
+            Contact.findById(req.params.id, function(err, doc) {
+                if(!err) {
+                    res.status(200).json(doc);
+                } else {
+                    handleError(res, err.message, "Failed to get contact");
+                }
+            });
+        }
+    },
+
+    /**
+     * Updates details for contact with specific ID
+     */
+    update: function(req, res) {
+        if(!ObjectId.isValid(req.params.id)) {
+            handleError(res, "Invalid ID", `No contact with given ID: ${req.params.id}`, 400);
+        } else {
+            let contact = {
+                name: req.body.name,
+                email: req.body.email,
+                location: req.body.location,
+                primary: req.body.primary
+            };
+            Contact.findByIdAndUpdate(req.params.id, { $set: contact}, {new: true}, function(err, doc) {
+                if(!err) {
+                    res.status(200).json(doc);
+                } else {
+                    handleError(res, err.message, "Failed to update contact");
+                }
+            });
+        }
+    },
+
+    /**
+     * Deletes contact with specific ID
+     */
+    delete: function(req, res) {
+        if(!ObjectId.isValid(req.params.id)) {
+            handleError(res, "Invalid ID", `No contact with given ID: ${req.params.id}`, 400);
+        } else {
+            Contact.findByIdAndDelete(req.params.id, function(err, result) {
+               if(!err) {
+                   res.status(200).json(req.params.id);
+               } else {
+                   handleError(res, err.message, "Failed to delete contact");
+               }
+            });
+        }
     }
 }
--- express-app/routes/routes.js
+++ express-app/routes/routes.js
@@ -6,10 +6,16 @@ const contactController = require("../controllers/contactController");
 /**
  * Creates endpoints mapped to appropriate controller functions.
  * 
- * GET "/contacts/"     returns all contacts
- * POST "/contact/"     creates a new contact
+ * GET      "/contacts/"        returns all contacts
+ * POST     "/contacts/"        creates a new contact
+ * GET      "/contacts/:id"     returns contact by ID
+ * PUT      "/contacts/:id"     updates contact found by ID
+ * DELETE   "/contacts/:id"     deletes contact found by ID
  */
 router.get("/contacts/", contactController.all);
 router.post("/contacts/", contactController.create);
+router.get("/contacts/:id", contactController.getById);
+router.put("/contacts/:id", contactController.update);
+router.delete("/contacts/:id", contactController.delete);
 
 module.exports = router;
