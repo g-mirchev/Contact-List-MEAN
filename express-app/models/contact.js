9244b7b2017bce9d429c22651e4ac2aaa0fcf395
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-04 15:42:12 +0100

    Better comments

--- express-app/models/contact.js
+++ express-app/models/contact.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+/**
+ * Creates schema for Contact model.
+ * 
+ * @param {String} name The contact name is required
+ * @param {String} email The contact email is required
+ * @param {String} location The contact location is required
+ * @param {String} primary The contact phone number is required
+ */
 const contactSchema = new mongoose.Schema({
     name: { 
         type: String,
@@ -19,6 +27,9 @@ const contactSchema = new mongoose.Schema({
     }
 });
 
+/**
+ * Builds contact model from contact schema.
+ */
 let Contact = mongoose.model("Contact", contactSchema);
 
 module.exports = { Contact };
