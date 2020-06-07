0c632d1a1ed2945860acef931733fc14351f7e2f
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-07 14:58:58 +0100

    Added cors headers for debugging

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const app = express();
  * Mount bodyParses and router to app.
  */
 app.use(bodyParser.json());
+// Add cors headers for debugging
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+    res.setHeader('Access-Control-Allow-Credential', true);
+    next();
+});
 app.use("/api", routes);
 
 /**
