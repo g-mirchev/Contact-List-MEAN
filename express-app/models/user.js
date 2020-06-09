11c6d96b15c25e6960c60b9568d187e7d5c2650f
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-09 19:04:58 +0100

    added extra validators

--- express-app/models/user.js
+++ express-app/models/user.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema({
     salt: String
 });
 
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail');
 /**
  * Generates a salt(string of unique characters) then
  * creates a hash with the given password and hash to avoid
