1b71a77c930387c98a38517083147a459b8d5df3
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 13:59:54 +0100

    added post method

--- angular-app/src/app/shared/services/user.service.ts
+++ angular-app/src/app/shared/services/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
+  selectedUser: User = {
+    name: '',
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Makes an API POST request to register a user.
+   * 
+   * @param user  the user information as object
+   */
+  postUser(user: User){
+    return this.http.post(environment.apiBaseUrl + '/register', user);
+  }
 }
