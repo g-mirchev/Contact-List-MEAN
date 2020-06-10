04a1412ce2a47d3d447a4fb259967ae23c059c81
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 21:22:06 +0100

    removed import

--- angular-app/src/app/shared/services/user.service.ts
+++ angular-app/src/app/shared/services/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user.model';
 import { environment } from '../../../environments/environment';
-import { relative } from 'path';
 
 @Injectable()
 export class UserService {
