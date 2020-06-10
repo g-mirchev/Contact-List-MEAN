eea0006775a830b54336e01f067a4e1c2ea33e3a
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 14:52:57 +0100

    edited passport local strategy

--- express-app/config/passport.js
+++ express-app/config/passport.js
@@ -8,25 +8,27 @@ passport.use(new LocalStrategy({
     // set the username field to be 'email' instead of the default 'username'
     usernameField: 'email'
 },
-    function(username, password, done) {
-        User.findOne({ email: username }, function (err, user) {
+    (username, password, done) => {
+        User.findOne({ email: username }, (err, user) => {
             if(err) {
                 return done(err);
             }
-            // Return if the user isnot found in the database.
-            if(!user) {
+            // Return if the user is not found in the database.
+            else if(!user) {
                 return done(null, false, {
                     message: 'User not found'
                 });
             }
             // Return if password is wrong, uses schema defined function for check
-            if(!user.validPassword(password)) {
+            else if(!user.validPassword(password)) {
                 return done(null, false, {
                     message: 'Password is wrong'
                 });
             }
             // Return the user object if all crecentials are correct.
-            return done(null, user);
+            else {
+                return done(null, user);
+            }
         });
     }
 ));
