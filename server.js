fd9837cfbf9bb34b5b3f820b1faf182aa754699d
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 20:09:07 +0100

    updated cors policy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(passport.initialize());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, noauth, Authotization');
     res.setHeader('Access-Control-Allow-Credential', true);
     next();
 });
