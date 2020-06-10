18a0cbcfd92a647ed983af91e883bc3379976a4d
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 14:07:23 +0100

    added route to contacts section

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user/user.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,9 @@ const routes: Routes = [
   },
   {
     path: '', redirectTo: '/signup', pathMatch: 'full'
+  },
+  {
+    path: 'contacts', component: ContactListComponent
   }
 ];
 
