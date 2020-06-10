63fa250ea9c0360a26004222c4095a4fcff6e169
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 19:28:06 +0100

    added authentication guard

--- angular-app/src/app/auth/auth.guard.ts
+++ angular-app/src/app/auth/auth.guard.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { UserService } from '../shared/services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  /**
+   * Calls userService.isLoggedIn to check for a valid token, if no such found 
+   * or token has expired, redirects to login page deletes token from local storage.
+   */
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): boolean {
+      if(!this.userService.isLoggedIn()) {
+        this.router.navigateByUrl('/login');
+        this.userService.deleteToken();
+        return false;
+      }
     return true;
   }
   
