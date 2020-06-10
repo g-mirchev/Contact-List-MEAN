bd5cf38cd53a01a2d638339793ee1660f263d3d1
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 14:00:58 +0100

    imported HttpClientModule

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,22 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule} from '@angular/common/http';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { UserComponent } from './user/user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactDetailsComponent,
-    ContactListComponent
+    ContactListComponent,
+    UserComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
