a529340c1582cb7043a7dd08b654d83b13328ff2
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-10 16:15:14 +0100

    Added Jason Web Token verification handling

---
+++ express-app/config/jwtHelper.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+/**
+ * Calls jsonwebtoken.verify if the current request has an 
+ * 'authorization' header.
+ */
+module.exports.verifyJwt = function(req, res, next) {
+    let token;
+    if('authorization' in req.headers) {
+        token = req.headers['authorization'].split(' ')[1];
+    }
+    if(!token) {
+        return res.status(403).send({auth: false, message: 'Missing token.'});
+    }
+    else {
+        jwt.verify(token, process.env.JWT_SECRET,
+            (err, decoded) => {
+                if(err) {
+                    return res.status(500).send({auth: false, message: 'Token authorization failed.'});
+                }
+                else {
+                    req._id = decoded._id;
+                    next();
+                }
+            });
+    }
+}
