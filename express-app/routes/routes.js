5e6fa0a42be5b97c344d0b35a578f5a1a41ed0e7
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-06-11 12:14:57 +0100

    removed unused route

--- express-app/routes/routes.js
+++ express-app/routes/routes.js
@@ -11,13 +11,11 @@ const jwtHelper = require('../config/jwtHelper');
  * 
  * GET      "/contacts/"        returns all contacts
  * POST     "/contacts/"        creates a new contact
- * GET      "/contacts/:id"     returns contact by ID
  * PUT      "/contacts/:id"     updates contact found by ID
  * DELETE   "/contacts/:id"     deletes contact found by ID
  */
 router.get("/contacts/", jwtHelper.verifyJwt, contactController.all);
 router.post("/contacts/", contactController.create);
-router.get("/contacts/:id", contactController.getById);
 router.put("/contacts/:id", contactController.update);
 router.delete("/contacts/:id", contactController.delete);
 router.post("/register/", userController.register);
